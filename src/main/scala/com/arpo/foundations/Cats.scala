83a2a5f99eeadbdee9e76a774d17127e08072c04
agoralewski <1arturro1@gmail.com>
2024-01-07 16:41:05 +0100

    Cats + Semigroupal

--- src/main/scala/com/arpo/foundations/Cats.scala
+++ src/main/scala/com/arpo/foundations/Cats.scala
@@ -6,9 +6,13 @@ object Cats {
 
   /*
 
-    Semigroup[A] -> Monoid
-    Eq
-    Functor
+    Semigroup[A]{combine} -> Monoid{empty}
+
+    Eq{eqv}
+
+    Functor[F[_]]{map}
+
+    Semigroupal{product}
 
    */
 
@@ -25,11 +29,12 @@ object Cats {
     def neqv(x: A, y: A): Boolean // alias =!=
   }
 
-
-
   trait MyFunctor[F[_]] {
     def map[A, B](initialValue: F[A])(f: A => B): F[B]
   }
 
+  trait MySemigroupal[F[_]] {
+    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
+  }
 
 }
